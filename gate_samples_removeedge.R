674f78defea55b781057ed2fb7786da0bf57ec0f
Max Zhao <alcasa.mz@gmail.com>
2020-03-31 11:05:57 +0200

    Adding experiments with removal of edge events

---
+++ gate_samples_removeedge.R
@@ -0,0 +1,93 @@
+library(flowCore)
+library(flowDensity)
+
+# load dataset into R by simply listing all files in input directory and saving
+# them again into the output directory
+
+kInput <- "output/samples"
+kOutput <- "output/gated_single_removeedge"
+
+kChannels1 <- c("CD45-KrOr", "SS INT LIN")
+kPosition1 <- c(T, F)
+kChannels2 <- c("CD19-APCA750", "SS INT LIN")
+kPosition2 <- c(T, F)
+
+inputFiles <- sapply(list.files(kInput, recursive = T, pattern = "\\.LMD$"), function(p) {file.path(kInput, p)})
+
+logTransformList <- transformList(sapply(1:10, function(i){sprintf("FL%d INT LOG", i)}),
+                                  logTransform())
+
+GateSample <- function(inputData, plotPath) {
+  chNames <- inputData@parameters@data$name
+  names(chNames) <- inputData@parameters@data$desc
+  chKeys1 <- as.character(chNames[kChannels1])
+  chKeys2 <- as.character(chNames[kChannels2])
+  gatedData1 <- flowDensity(inputData, channels = chKeys1, position = kPosition1)
+  # gatedData2 <- flowDensity(gatedData1, channels = chKeys2, position = kPosition2, use.percentile = c(F, F), percentile = c(.25, .95))
+
+  jpeg(file.path(plotPath, "gate1.jpeg"))
+    plot(inputData, gatedData1)
+  dev.off()
+  # jpeg(file.path(plotPath, "gate2.jpeg"))
+  #   plot(inputData, gatedData2)
+  # dev.off()
+
+  getflowFrame(gatedData1)
+}
+
+LoadSample <- function(inputPath) {
+  cat(inputPath, "\n")
+  # load data linearized
+  data <- read.FCS(inputPath, transformation = "linearize", dataset = 1)
+
+  # apply log scaling to all channels besides SS and FS
+  data <- transform(data, logTransformList)
+  data
+}
+
+SaveSample <- function(data, outputPath) {
+  write.FCS(data, outputPath)
+}
+
+# Uses edge event code from: https://github.com/SofieVG/FloReMi
+FilterEdgeEvents <- function(data, dimensions) {
+
+  chNames <- data@parameters@data$name
+  names(chNames) <- data@parameters@data$desc
+
+  # Look op the accepted ranges for the dimensions
+  meta <- pData(data@parameters)
+  rownames(meta) <- meta[,"name"]
+  selection <- rep(TRUE,times=dim(data)[1])
+
+  e <- exprs(data)
+  # Make selection
+  for(dd in dimensions){
+    d <- as.character(chNames[dd])
+    selection <- selection &
+      e[,d] > max(meta[d,"minRange"],min(e[,d])) &
+      e[,d] < min(meta[d,"maxRange"],max(e[,d]))
+  }
+  return(data[selection,])
+}
+
+ProcessSample <- function(inputPath, outputDir) {
+  outputPath <- file.path(outputDir, substr(inputPath, nchar(kInput) + 2, nchar(inputPath) + 1))
+  plotPath <- file.path(outputDir, "plots", substr(inputPath, nchar(kInput) + 2, nchar(inputPath) + 1))
+  plotPath <- gsub(".LMD", "", plotPath)
+
+  dir.create(dirname(outputPath), recursive = T, showWarnings = F)
+  dir.create(plotPath, recursive = T, showWarnings = F)
+
+  inputData <- LoadSample(inputPath)
+  inputData <- FilterEdgeEvents(inputData, c("SS INT LIN", "FS INT LIN", "CD45-KrOr"))
+  tryCatch({
+    processedData <- GateSample(inputData, plotPath)
+    SaveSample(processedData, outputPath)
+  }, error = function(e) {
+    show(e)
+  })
+}
+
+# ProcessSample(inputFiles[1], kOutput)
+lapply(inputFiles, function(f) { ProcessSample(f, kOutput) })
---
+++ train_classifier_gated_removeedge.py
@@ -0,0 +1,72 @@
+"""
+Train a flowcat classifier including SOMs for the given dataset.
+
+Use the dataset that has been gated a single time on SS INT LIN and CD45 Kr-Or
+"""
+import sys
+from flowcat import io_functions, utils, flowcat, constants
+from flowcat.seed import set_seed
+
+IDENTIFIER = sys.argv[1]
+NAME = f"classifier_gated_removeedge_{IDENTIFIER}"
+
+def check_dataset(dataset: "CaseCollection"):
+    """Check that cases can be loaded."""
+    LOGGER.info(dataset)
+    case = dataset[0]
+    LOGGER.info(case)
+    sample = case.get_tube("1")
+    LOGGER.info("Tube 1 %s", sample)
+    data = sample.get_data()
+    LOGGER.info(data)
+    LOGGER.info(f"{data.data.shape}, {data.data.min(axis=1)}, {data.data.max(axis=1)}")
+
+def filter_missing_files(dataset: "CaseCollection") -> "CaseCollection":
+    LOGGER.info("Before filtering: %s", dataset)
+
+    def all_samples_exist(case: "Case") -> bool:
+        return all(s.complete_path.exists() for s in case.samples)
+
+    filtered = dataset.copy()
+    filtered.cases = [
+        case for case in filtered.cases if all_samples_exist(case)
+    ]
+    filtered.data_path = dataset.data_path
+    LOGGER.info("After filtering only existing: %s", filtered)
+    return filtered
+
+SEED = None
+OUTPUT = utils.URLPath(f"output/{NAME}")
+LOGDIR = utils.URLPath(f"logs/{NAME}_{utils.create_stamp()}")
+INPUT = {
+    "data": utils.URLPath("output/gated_single_removeedge/data"),
+    "meta": utils.URLPath("output/samples/meta.json.gz"),
+}
+
+LOGGER = utils.setup_logging(LOGDIR, NAME)
+
+set_seed(SEED)
+dataset = io_functions.load_case_collection(INPUT["data"], INPUT["meta"])
+
+check_dataset(dataset)
+
+# remove all cases without filtered fcs
+filtered_dataset = filter_missing_files(dataset)
+
+train, test = filtered_dataset.create_split(0.9)
+io_functions.save_json(train.labels, OUTPUT / "train_ids.json")
+io_functions.save_json(test.labels, OUTPUT / "test_ids.json")
+
+reference = train.sample(1)
+LOGGER.info("Reference dataset: %s", reference)
+LOGGER.info("Reference labels: %s", reference.labels)
+
+model = flowcat.FlowCat()
+args = constants.DEFAULT_TRAIN_ARGS
+args["classifier"]["balance"] = None
+args["classifier"]["split_ratio"] = 1.0
+args["classifier"]["config"].tubes = ["1", "2"]
+args["classifier"]["config"].train_epochs = 150
+som_train, som_test = model.train(train, reference, OUTPUT, args=args, validation_data=test)
+predictions = model.predict_dataset(som_test)
+preds = model.predictions_to_metric(predictions, OUTPUT / "preds")
